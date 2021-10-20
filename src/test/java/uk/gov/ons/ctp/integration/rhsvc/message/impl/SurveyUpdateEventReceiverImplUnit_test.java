7dff38940f95d024ff44d6be270acd6aca299b14
KieranWardle <23309814+KieranWardle@users.noreply.github.com>
2021-10-20 15:00:43 +0100

    SOCINT-186 Added unit tests

--- src/test/java/uk/gov/ons/ctp/integration/rhsvc/message/impl/SurveyUpdateEventReceiverImplUnit_test.java
+++ src/test/java/uk/gov/ons/ctp/integration/rhsvc/message/impl/SurveyUpdateEventReceiverImplUnit_test.java
@@ -1,13 +1,16 @@
 package uk.gov.ons.ctp.integration.rhsvc.message.impl;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.ons.ctp.common.FixtureHelper;
+import uk.gov.ons.ctp.common.error.CTPException;
 import uk.gov.ons.ctp.common.event.model.SurveyUpdateEvent;
 import uk.gov.ons.ctp.integration.rhsvc.event.impl.SurveyEventReceiverImpl;
 import uk.gov.ons.ctp.integration.rhsvc.repository.RespondentDataRepository;
@@ -25,5 +28,15 @@ public class SurveyUpdateEventReceiverImplUnit_test {
         FixtureHelper.loadPackageFixtures(SurveyUpdateEvent[].class).get(0);
     target.acceptSurveyUpdateEvent(surveyUpdateEvent);
     verify(mockRespondentDataRepo).writeSurvey(surveyUpdateEvent.getPayload().getSurveyUpdate());
+  }
+
+  @Test
+  public void test_acceptSurveyEvent_exceptionThrown() throws Exception {
+    SurveyUpdateEvent surveyUpdateEvent =
+        FixtureHelper.loadPackageFixtures(SurveyUpdateEvent[].class).get(0);
+    Mockito.doThrow(new CTPException(CTPException.Fault.RESOURCE_NOT_FOUND))
+        .when(mockRespondentDataRepo)
+        .writeSurvey(surveyUpdateEvent.getPayload().getSurveyUpdate());
+    assertThrows(CTPException.class, () -> target.acceptSurveyUpdateEvent(surveyUpdateEvent));
   }
 }
