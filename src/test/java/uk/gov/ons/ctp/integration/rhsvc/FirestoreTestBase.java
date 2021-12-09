8c200d046a37e2c9d8b6758acb538550892fc344
Rob Catling <github@rpc-soft.com>
2021-12-09 11:56:05 +0000

    SOCINT-291 control firestore replacement for each new test class

--- src/test/java/uk/gov/ons/ctp/integration/rhsvc/FirestoreTestBase.java
+++ src/test/java/uk/gov/ons/ctp/integration/rhsvc/FirestoreTestBase.java
@@ -2,10 +2,13 @@ package uk.gov.ons.ctp.integration.rhsvc;
 
 import static org.junit.jupiter.api.parallel.ResourceAccessMode.READ_WRITE;
 
-import com.google.api.gax.core.CredentialsProvider;
-import com.google.api.gax.core.NoCredentialsProvider;
+import com.google.cloud.NoCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.FirestoreOptions;
 import com.google.cloud.spring.pubsub.core.PubSubTemplate;
 import com.google.cloud.spring.pubsub.integration.inbound.PubSubInboundChannelAdapter;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.parallel.ResourceLock;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +17,14 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+import uk.gov.ons.ctp.common.cloud.FirestoreProvider;
 import uk.gov.ons.ctp.common.event.EventPublisher;
 import uk.gov.ons.ctp.common.firestore.TestCloudDataStore;
 import uk.gov.ons.ctp.common.utility.ParallelTestLocks;
@@ -37,43 +40,81 @@ import uk.gov.ons.ctp.common.utility.ParallelTestLocks;
 @MockBean(name = "collectionExerciseEventInbound", value = PubSubInboundChannelAdapter.class)
 @ResourceLock(value = ParallelTestLocks.SPRING_TEST, mode = READ_WRITE)
 @Tag("firestore")
+@Slf4j
 public abstract class FirestoreTestBase {
-  private static final String GCP_PROJECT = "sdc-rh-test";
+  private static final String GCP_PROJECT = "rh-testcontainers";
   protected static final String CASE_SCHEMA = GCP_PROJECT + "-case";
   protected static final String UAC_SCHEMA = GCP_PROJECT + "-uac";
   protected static final String COLLEX_SCHEMA = GCP_PROJECT + "-collection_exercise";
   protected static final String SURVEY_SCHEMA = GCP_PROJECT + "-survey";
 
-  @Autowired protected TestCloudDataStore dataStore;
+  @Autowired protected TestCloudDataStore testDataStore;
+
+  private static TestFirestoreProvider provider;
 
   @Container
   private static final CustomFirestoreEmulatorContainer firestoreEmulator =
       new CustomFirestoreEmulatorContainer(
           DockerImageName.parse("gcr.io/google.com/cloudsdktool/cloud-sdk:317.0.0-emulators"));
 
-  @DynamicPropertySource
-  static void emulatorProperties(DynamicPropertyRegistry registry) {
-    registry.add("spring.cloud.gcp.firestore.host-port", firestoreEmulator::getEmulatorEndpoint);
-    registry.add("spring.cloud.gcp.firestore.project-id", () -> GCP_PROJECT);
+  @BeforeAll
+  public static void classSetup() {
+    log.info("Initialising test firestoreProvider");
+    if (provider == null) {
+      provider = new TestFirestoreProvider();
+    }
+    provider.init(GCP_PROJECT, firestoreEmulator);
   }
 
   @TestConfiguration
   static class EmulatorConfiguration {
-    // By default, autoconfiguration will initialize application default credentials.
-    // For testing purposes, don't use any credentials. Bootstrap w/ NoCredentialsProvider.
     @Bean
-    CredentialsProvider googleCredentials() {
-      return NoCredentialsProvider.create();
+    @Primary
+    FirestoreProvider firestoreProvider() {
+      return provider;
     }
   }
 
   protected void deleteAllCollections() {
-    dataStore.deleteCollection(SURVEY_SCHEMA);
-    dataStore.deleteCollection(COLLEX_SCHEMA);
-    dataStore.deleteCollection(UAC_SCHEMA);
-    dataStore.deleteCollection(CASE_SCHEMA);
+    testDataStore.deleteCollection(SURVEY_SCHEMA);
+    testDataStore.deleteCollection(COLLEX_SCHEMA);
+    testDataStore.deleteCollection(UAC_SCHEMA);
+    testDataStore.deleteCollection(CASE_SCHEMA);
   }
 
+  @Slf4j
+  public static class TestFirestoreProvider implements FirestoreProvider {
+    private Firestore firestore;
+
+    public TestFirestoreProvider() {
+      log.info("Creating new TestFirestoreProvider");
+      System.out.println("CREATING NEW TestFirestoreProvider");
+    }
+
+    public TestFirestoreProvider init(
+        String gcpProject, CustomFirestoreEmulatorContainer emulator) {
+      log.info("Creating emulator firestore instance with projectId {}", gcpProject);
+      System.out.println(
+          "CREATING EMULATOR FIRESTORE: "
+              + gcpProject
+              + " on endpoint: "
+              + emulator.getEmulatorEndpoint());
+      firestore =
+          FirestoreOptions.newBuilder()
+              .setProjectId(gcpProject)
+              .setHost(emulator.getEmulatorEndpoint())
+              .setCredentials(NoCredentials.getInstance())
+              .build()
+              .getService();
+      return this;
+    }
+
+    public Firestore get() {
+      return firestore;
+    }
+  }
+
+  @Slf4j
   public static class CustomFirestoreEmulatorContainer
       extends GenericContainer<CustomFirestoreEmulatorContainer> {
 
@@ -86,6 +127,10 @@ public abstract class FirestoreTestBase {
 
     public CustomFirestoreEmulatorContainer(final DockerImageName dockerImageName) {
       super(dockerImageName);
+
+      System.out.println("SYSOUT STARTING FIRESTORE EMULATOR !");
+
+      log.info("STARTING FIRESTORE EMULATOR");
 
       dockerImageName.assertCompatibleWith(DEFAULT_IMAGE_NAME);
 
